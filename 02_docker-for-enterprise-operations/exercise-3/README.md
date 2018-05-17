8921dbbc010db1d7c4410f73fb46dbb7016aa29b
Michael Lihs (CI/OSC1) <michael.lihs@de.bosch.com>
2018-05-17 11:12:24 +0200

    update exercise 3

--- 02_docker-for-enterprise-operations/exercise-3/README.md
+++ 02_docker-for-enterprise-operations/exercise-3/README.md
@@ -16,3 +16,13 @@ Install `kubectl` on Mac
 Run
 
     brew install kubectl
+
+
+Run commands against API
+========================
+
+* get all containers
+
+   ```
+   ucp-api "https://${UCP_FQDN}/containers/json" | jq
+   ```
--- 02_docker-for-enterprise-operations/exercise-3/script.sh
+++ 02_docker-for-enterprise-operations/exercise-3/script.sh
@@ -9,3 +9,16 @@ brew install kubectl
 kubectl
 kubectl create -f pod.yml
 curl -s --cacert ./ca.pem --key ./key.pem --cert ./cert.pem https://${UCP_FQDN}:6443/api/v1/pods
+
+
+ucp-api "https://${UCP_FQDN}/containers/json" | jq
+touch mysecret
+ucp-api --data '{"Data":"'$(base64 mysecret)'","Name":"test_secret"}' -X POST "https://${UCP_FQDN}/secrets/create"
+ucp-api -X DELETE "https://${UCP_FQDN}/services/phh534k2d0m5qmsoltd7cn06y"
+touch myservice.json
+ucp-api --data @myservice.json "https://${UCP_FQDN}/services/create"
+cp myservice.json myservice-update.json
+
+# get Version.Index and add it as ?version=298
+ucp-api "https://${UCP_FQDN}/services/g00cep8t5spjdvbc9wo82qwux" | jq
+ucp-api --data @myservice-update.json "https://${UCP_FQDN}/services/g00cep8t5spjdvbc9wo82qwux/update?version=298"
