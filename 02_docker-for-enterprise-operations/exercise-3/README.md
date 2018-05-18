14b824e14b5522f8a085ac0846bba1b3947a6622
Michael Lihs (CI/OSC1) <michael.lihs@de.bosch.com>
2018-05-18 10:36:11 +0200

    fix kubernetes login

--- 02_docker-for-enterprise-operations/exercise-3/README.md
+++ 02_docker-for-enterprise-operations/exercise-3/README.md
@@ -5,15 +5,11 @@ Run
 
     source ./login.sh
 
-Afterwards you can set up the following alias:
-
-    alias ucp-api='curl -k -H "Authorization: Bearer $AUTHTOKEN"'
-
 Get bundle with
 
     ucp-api https://${UCP_FQDN}/api/clientbundle -o bundle.zip
     unzip bundle.zip
-    ./
+    eval "$(<env.sh)"
 
 
 Install `kubectl` on Mac
--- 02_docker-for-enterprise-operations/exercise-3/login.sh
+++ 02_docker-for-enterprise-operations/exercise-3/login.sh
@@ -4,3 +4,7 @@ export UCP_FQDN="206.189.54.85"
 export AUTHTOKEN=$(curl -sk -d '{"username":"admin","password":"adminadmin"}' https://${UCP_FQDN}/auth/login | jq -r .auth_token)
 echo "AUTHTOKEN: ${AUTHTOKEN}"
 alias ucp-api='curl -k -H "Authorization: Bearer $AUTHTOKEN"'
+
+ucp-api https://${UCP_FQDN}/api/clientbundle -o bundle.zip
+unzip bundle.zip
+eval "$(<env.sh)"
