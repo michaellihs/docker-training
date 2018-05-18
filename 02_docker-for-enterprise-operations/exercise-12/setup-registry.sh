7ce7ea67aaa858c87e95f60b80c6210aa093ab6f
Michael Lihs (CI/OSC1) <michael.lihs@de.bosch.com>
2018-05-18 13:28:28 +0200

    exercise 2-12

---
+++ 02_docker-for-enterprise-operations/exercise-12/setup-registry.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DTR_FQDN=bos247
+UCP_IP=206.189.54.85
+
+docker run -it --rm docker/dtr:2.5.0 install \
+  --ucp-node ucp-manager-0 \
+  --ucp-username admin \
+  --ucp-password adminadmin \
+  --ucp-url https://${UCP_IP} \
+  --ucp-insecure-tls \
+  --replica-https-port 4443 \
+  --replica-http-port 81 \
+  --dtr-external-url https://${DTR_FQDN}:4443
