741ebbf0a3a588babd65600edb5898afd0af9846
Michael Lihs (CI/OSC1) <michael.lihs@de.bosch.com>
2018-05-18 15:56:08 +0200

    exercise 2-12

--- 02_docker-for-enterprise-operations/exercise-12/create-secrets.sh
+++ 02_docker-for-enterprise-operations/exercise-12/create-secrets.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "access-example" | docker secret create access_key -
-echo "secret-example" | docker secret create secret_key -
+echo "secret-example" | docker secret create secret_key -
